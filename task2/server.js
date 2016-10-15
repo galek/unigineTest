01697a49784ed5a2a4b09026e8dc57b93c468c39
leonid-ilin <leonid_ilin8@mail.ru>
2016-10-15 19:35:03 +0700

    Add some functionality to joystick-app using nipplejs library. Websocket nodejs server was created on expressjs framework using socket.io library for websockets

--- task2/server.js
+++ task2/server.js
@@ -3,19 +3,30 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-
-app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/jquery', express.static(__dirname + '/../node_modules/jquery/dist'));
+app.use('/nipplejs', express.static(__dirname + '/../node_modules/nipplejs/dist'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  console.log('Joystick connected!');
+  
+  socket.on('joystick event', function(data){
+    // for (var i = 0; i < data[0].length; i++) {
+    //   data[0][i]=0;
+    // }
+    console.log("Stick 1: " + data[0]);
+    console.log("Buttons 2: " + data[1]);
+    console.log("Distance: " + data[2]);
   });
+  socket.on('disconnect', function(){
+    console.log('Joystick disconnected');
+  });  
+  //socket.disconnect();
 });
 
 http.listen(port=80, function(){
   console.log('listening on *:'+port);
-});
+}); 
